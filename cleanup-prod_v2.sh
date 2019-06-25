cb7129dd16baee46eb05eda66961143129c90ee5
meggarr <meggarr@users.noreply.github.com>
2019-06-25 11:18:21 +0800

    Update cleanup-prod_v2.sh

--- cleanup-prod_v2.sh
+++ cleanup-prod_v2.sh
@@ -3,5 +3,5 @@
 _DIR=`dirname $0`
 _DIR=`cd $_DIR; pwd`
 
-node ${_DIR}/cleanup-v2.js -m mongodb://199.71.142.249:27017,199.71.143.62:27017,199.71.143.63:27017/?replicaSet=cmdctr --acs-url http://gcs.x.com:8081 $@
+node ${_DIR}/cleanup-v2.js -m mongodb://127.0.0.1:27017 --acs-url http://gcs.x.com:8081 $@
 
